63ae2612728baa2045de4698a784c0c361cc0449
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-10-24 14:18:35 -0300

    fundamentos reactDom css e JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// import React que faz o arquivo entender JSX, sem ele náo funciona
+import ReactDOM from "react-dom/client";
+// import ReactDOM faz com que o id= root funcione
+import "./index.css";
+// importa o arquivo .css
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const tag = <strong>Olá React!!</strong>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div> {tag} </div>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
